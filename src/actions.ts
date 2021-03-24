07a2784ddf288b1b3e3b6bd24d847aea9d0e162a
itstudent17 <itstudent17@outlook.com>
2021-03-24 22:19:56 +0300

    constants for action types

--- src/actions.ts
+++ src/actions.ts
@@ -1,6 +1,8 @@
-export type AddNoteActionType = { type: "ADD_NOTE"; payload: string };
+import { ADD_NOTE } from "./actionsTypes";
+
+export type AddNoteActionType = { type: string; payload: string };
 
 export const addNote = (note: string): AddNoteActionType => ({
-  type: "ADD_NOTE",
+  type: ADD_NOTE,
   payload: note,
 });
