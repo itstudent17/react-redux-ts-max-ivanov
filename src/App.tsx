05719acb7f2f17203374d2a4c211e0c1bbad01b1
itstudent17 <itstudent17@outlook.com>
2021-03-24 09:29:13 +0300

    Provider with store prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { NewNoteInput } from "./NewNoteInput";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <NewNoteInput addNote={() => alert()} />
       <hr />
       <ul>
         <li>Some note</li>
       </ul>
-    </>
+    </Provider>
   );
 }
 
