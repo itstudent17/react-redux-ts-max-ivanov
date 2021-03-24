1a31bb72e9f3c7c7ee4000c3966c1a0e07f19fde
itstudent17 <itstudent17@outlook.com>
2021-03-24 09:00:03 +0300

    stote and reducer added

---
+++ src/reducer.ts
@@ -0,0 +1,13 @@
+const initialState = {
+  notes: [],
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_NOTE":
+      return { ...state, notes: [...state.notes, action.payload] };
+
+    default:
+      return state;
+  }
+};
