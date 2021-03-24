5e37679e30339205165e850b415ac02a4a1adfda
itstudent17 <itstudent17@outlook.com>
2021-03-24 09:22:42 +0300

    validation of state and action

--- src/reducer.ts
+++ src/reducer.ts
@@ -6,7 +6,12 @@ const initialState = {
   notes: [],
 };
 
-export const reducer = (state: NotesState = initialState, action) => {
+type AddNoteActionType = { type: "ADD_NOTE"; payload: string };
+
+export const reducer = (
+  state: NotesState = initialState,
+  action: AddNoteActionType
+) => {
   switch (action.type) {
     case "ADD_NOTE":
       return { ...state, notes: [...state.notes, action.payload] };
