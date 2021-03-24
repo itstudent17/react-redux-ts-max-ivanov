e087549f12c3369f90df3b35c0ed2f7f55a89c66
itstudent17 <itstudent17@outlook.com>
2021-03-24 08:35:03 +0300

    Added state and handler

--- src/NewNoteInput.tsx
+++ src/NewNoteInput.tsx
@@ -1,11 +1,24 @@
+import React, { useState } from "react";
+
 interface NewNoteInputProps {
   addNote: (note: string) => void;
 }
 
 export const NewNoteInput: React.FC<NewNoteInputProps> = ({ addNote }) => {
+  const [note, setNote] = useState<string>("");
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNote(event.target.value);
+  };
+
   return (
     <div>
-      <input type="text" name="note" placeholder="Enter your note" />
+      <input
+        type="text"
+        name="note"
+        placeholder="Enter your note"
+        value={note}
+        onChange={handleChange}
+      />
       <button onClick={() => alert()}>Add note</button>
     </div>
   );
